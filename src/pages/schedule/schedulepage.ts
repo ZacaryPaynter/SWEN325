3feb9a6d18460e6d9bbc930673fe2d4a223ab46b
escottkiri <escottkiri@ecs.vuw.ac.nz>
2018-08-30 20:14:02 +1200

    front end design changes to schedule page

--- src/pages/schedule/schedulepage.ts
+++ src/pages/schedule/schedulepage.ts
@@ -1,23 +1,27 @@
-import { Component, ViewChild, AfterViewInit} from '@angular/core';
-import { NavController, Slides} from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides, FabContainer } from 'ionic-angular';
 import { NgRedux } from '../../../node_modules/ng2-redux';
 import { MyState } from '../../store/store';
 import { Schedule } from '../schedule/schedule';
 import { ScheduleService } from './schedule.service';
 import { SchedTime } from './schedtime';
-import {ScheduleDetail} from './schedule-detail';
+import { ScheduleDetail } from './schedule-detail';
 
 @Component({
   selector: 'page-schedule',
   templateUrl: 'schedulepage.html', 
   providers: [ScheduleService]
 })
-export class SchedulePage implements AfterViewInit{
+export class SchedulePage {
 
   @ViewChild(Slides) slides: Slides;
+  @ViewChild('fab') fab : FabContainer;
+  
   user: string;
   schedules: Schedule[];
   currentSchedule: Schedule;
+
+
 
   constructor(public navCtrl: NavController, private ngRedux: NgRedux<MyState>,
     private scheduleService: ScheduleService) {
@@ -29,11 +33,14 @@ export class SchedulePage implements AfterViewInit{
       this.schedules = schedule.map((schedule) => {
         return schedule;
       });
-    });
+    }); 
   }
 
-  ngAfterViewInit() {
-  }
+  ionViewDidLoad(){
+    this.fab.toggleList();
+ }
+
+
 
   editCurrentSchedule(sched: SchedTime){
     this.navCtrl.push(ScheduleDetail, {currentSchedule: this.currentSchedule, schedtime: sched});

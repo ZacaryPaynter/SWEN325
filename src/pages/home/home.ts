35e154d365b6a043bddf281792a8a4791ffe7044
Zacary <zacarypaynter@gmail.com>
2018-08-30 19:35:08 +1000

    work on kanban

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,16 +10,33 @@ import { MyState } from '../../store/store';
 export class HomePage {
   user : string;
   password : string;
-  tasks : any
+
+  todoList: any[];
+  doingList: any[];
+  doneList: any[];
 
   constructor(public navCtrl: NavController, private ngRedux: NgRedux<MyState>, public alertCtrl: AlertController) {
     this.user = this.ngRedux.getState().email;
-    //this.user = this.navParams.get('email');
     this.password = this.ngRedux.getState().password;
-    this.tasks =  [{title:"laundry", description:"do the laundry"}, {title:"cleaning", description:"clean the bathroom"}];
+
+    this.todoList =  [{title:"laundry", description:"do the laundry", open:false}, {title:"cleaning", description:"clean the bathroom", open:false}];
+    this.doingList =  [{title:"laundry2", description:"do the laundry2", open:false}, {title:"cleaning2", description:"clean the2 bathroom",  open:false}];
+    this.doneList =  [{title:"laundry3", description:"do the laundr3y", open:false}, {title:"cleani3ng", description:"clean the ba3throom", open:false}];
+    
     this.showAlert();
   }
 
+  public manageItem(x,i) {
+
+    if(x == 1){
+      this.todoList[i].open = !this.todoList[i].open;
+    }else if(x ==2){
+      this.doingList[i].open = !this.doingList[i].open;
+    }else{
+      this.doneList[i].open = !this.doneList[i].open;
+    }  
+    
+  }
 
   showAlert() {
     const alert = this.alertCtrl.create({
